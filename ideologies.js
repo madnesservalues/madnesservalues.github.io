23b4a45efcad22d54d3c128d07e768e0a0658e98
madnesservalues <ivanthebigman89@gmail.com>
2025-11-01 10:42:35 -0400

    v1.3.03

--- ideologies.js
+++ ideologies.js
@@ -15,11 +15,11 @@ ideologies = [
         "name": "Cashewism",
         "stats": {
             "char": 80,
-            "dipl": 20,
-            "govt": 35,
-            "scty": 34,
-            "dftn": 55,
-            "nrml": 35
+            "dipl": 10,
+            "govt": 30,
+            "scty": 30,
+            "dftn": 51,
+            "nrml": 30
         }
     },
     {
@@ -48,7 +48,7 @@ ideologies = [
         "name": "Esoteric Cashewism",
         "stats": {
             "char": 75,
-            "dipl": 20,
+            "dipl": 15,
             "govt": 25,
             "scty": 25,
             "dftn": 60,
@@ -67,17 +67,6 @@ ideologies = [
         }
     },
     {
-        "name": "Zadism",
-        "stats": {
-            "char": 32,
-            "dipl": 25,
-            "govt": 30,
-            "scty": 45,
-            "dftn": 40,
-            "nrml": 80
-        }
-    },
-    {
         "name": "Gordon Freemanism",
         "stats": {
             "char": 50,
@@ -86,6 +75,83 @@ ideologies = [
             "scty": 40,
             "dftn": 50,
             "nrml": 50
+        }
+    },
+    {
+        "name": "MadCon",
+        "stats": {
+            "char": 65,
+            "dipl": 45,
+            "govt": 70,
+            "scty": 35,
+            "dftn": 20,
+            "nrml": 70
+        }
+    },
+    {
+        "name": "MadLib",
+        "stats": {
+            "char": 85,
+            "dipl": 60,
+            "govt": 65,
+            "scty": 70,
+            "dftn": 15,
+            "nrml": 70
+        }
+    },
+    {
+        "name": "Neo-Contentitarian",
+        "stats": {
+            "char": 55,
+            "dipl": 48,
+            "govt": 51,
+            "scty": 70,
+            "dftn": 15,
+            "nrml": 70
+        }
+    },
+    {
+        "name": "Extilism",
+        "stats": {
+            "char": 20,
+            "dipl": 80,
+            "govt": 40,
+            "scty": 60,
+            "dftn": 70,
+            "nrml": 50
+        }
+    },
+    {
+        "name": "Bibertarianism",
+        "stats": {
+            "char": 60,
+            "dipl": 35,
+            "govt": 80,
+            "scty": 60,
+            "dftn": 30,
+            "nrml": 60
+        }
+    },
+    {
+        "name": "MadSock🧦", //
+        "stats": {
+            "char": 35,
+            "dipl": 85,
+            "govt": 80,
+            "scty": 70,
+            "dftn": 20,
+            "nrml": 60
+        }
+    },
+    {
+        "name": "Chauvinist",
+        "stats": {
+            "char": 25,
+            "dipl": 25,
+            "govt": 40,
+            "scty": 30,
+            "dftn": 35,
+            "nrml": 60
         }
     },
     {
