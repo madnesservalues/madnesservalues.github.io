91107a98b4829248eb38468d286d2fa280682943
madnesservalues <ivanthebigman89@gmail.com>
2025-11-04 19:05:26 -0500

    v1.3.04

--- ideologies.js
+++ ideologies.js
@@ -12,6 +12,17 @@ ideologies = [
         }
     },
     {
+        "name": "Withdrawn Centrism",
+        "stats": {
+            "char": 50,
+            "dipl": 45,
+            "govt": 40,
+            "scty": 40,
+            "dftn": 45,
+            "nrml": 50
+        }
+    },
+    {
         "name": "Cashewism",
         "stats": {
             "char": 80,
@@ -36,12 +47,23 @@ ideologies = [
     {
         "name": "Withdrawn Innovationist",
         "stats": {
-            "char": 85,
+            "char": 70,
             "dipl": 35,
             "govt": 60,
             "scty": 70,
             "dftn": 30,
             "nrml": 50
+        }
+    },
+    {
+        "name": "Absolute Innovationism",
+        "stats": {
+            "char": 10,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 95,
+            "dftn": 80,
+            "nrml": 100
         }
     },
     {
@@ -78,6 +100,17 @@ ideologies = [
         }
     },
     {
+        "name": "UltaContentitarian Biberty",
+        "stats": {
+            "char": 30,
+            "dipl": 25,
+            "govt": 70,
+            "scty": 25,
+            "dftn": 65,
+            "nrml": 70
+        }
+    },
+    {
         "name": "MadCon",
         "stats": {
             "char": 65,
@@ -97,6 +130,17 @@ ideologies = [
             "scty": 70,
             "dftn": 15,
             "nrml": 70
+        }
+    },
+    {
+        "name": "MadNat",
+        "stats": {
+            "char": 70,
+            "dipl": 65,
+            "govt": 40,
+            "scty": 45,
+            "dftn": 30,
+            "nrml": 50
         }
     },
     {
@@ -133,6 +177,17 @@ ideologies = [
         }
     },
     {
+        "name": "Flowictionism",
+        "stats": {
+            "char": 70,
+            "dipl": 40,
+            "govt": 60,
+            "scty": 55,
+            "dftn": 20,
+            "nrml": 35
+        }
+    },
+    {
         "name": "MadSock🧦", //
         "stats": {
             "char": 35,
@@ -152,6 +207,17 @@ ideologies = [
             "scty": 30,
             "dftn": 35,
             "nrml": 60
+        }
+    },
+    {
+        "name": "Magical Idealism",
+        "stats": {
+            "char": 25,
+            "dipl": 25,
+            "govt": 50,
+            "scty": 15,
+            "dftn": 90,
+            "nrml": 10
         }
     },
     {
